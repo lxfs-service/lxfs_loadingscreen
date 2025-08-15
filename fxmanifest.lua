bfac28c3b90331f5905a34b0b76646385ac0a580
LXFS - Service & Co <lxfs.service.co@gmail.com>
2025-08-15 17:01:04 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,6 @@
 fx_version 'cerulean'
 game 'gta5'
+version '1.0'
 
 author 'LXFS - Service'
 description 'LXFS-Loadingscreen'
@@ -10,4 +11,5 @@ files {
 }
 
 loadscreen_cursor("yes")
+
 loadscreen 'html/index.html'
