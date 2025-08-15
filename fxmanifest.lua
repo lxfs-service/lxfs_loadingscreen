6c5086d428ec1d76ac8d4e84334c7fcdacf20d0a
LXFS - Service & Co <lxfs.service.co@gmail.com>
2025-08-15 16:32:46 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'LXFS - Service'
+description 'LXFS-Loadingscreen'
+
+files {
+    "html/*.*",
+    "html/song.mp3"
+}
+
+loadscreen_cursor("yes")
+loadscreen 'html/index.html'
